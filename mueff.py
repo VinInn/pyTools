69765396b36a1e41f73f010edddae280c82cd6ed
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2015-05-17 13:43:09 +0200

    more histos

--- mueff.py
+++ mueff.py
@@ -21,9 +21,19 @@ def testBit(int_type, offset):
     mask = 1 << offset
     return(int_type & mask)
 
+def bitCount(int_type):
+    count = 0
+    while(int_type):
+        int_type &= int_type - 1
+        count += 1
+    return(count)
 
 
-# cvsF = open('/Users/innocent/data/mva/ttbar.csv')
+
+#sample= r'$t \bar t$'
+#cvsF = open('/Users/innocent/data/mva/ttbar.csv')
+
+sample='$J\psi$'
 cvsF = open('/Users/innocent/data/mva/jpsi.csv')
 
 
@@ -46,6 +56,8 @@ hp = header.index('hp')
 earlyMask =  int('1111111',2)
 promptMask = int(  '11111',2)
 
+def str2bool(v):
+  return str(v).lower() in ("yes", "true", "t", "1")
 
 def testAlgo(values,al) :
    return testBit(int(values[algoMask]),al)
@@ -54,15 +66,35 @@ def earlyAlgo(values) :
 def promptAlgo(values) :
     return int(values[algoMask]) & promptMask
 
+def countEarly(values) :
+    return bitCount(earlyAlgo(values))
+
 def fill(h,values,x):
     h.append(float(values[x]))
 
 npixH=[]
 n3dH=[]
 
+algoX=np.arange(0,12)
+algoH=np.zeros(12)
+oriAlgoH=np.zeros(12)
+earlyCountH=np.zeros(12)
+
+allAlgoH=np.zeros((12,12))
+def fillAll(values) :
+    am = int(values[algoMask])
+    for i in range(0,11) :
+        if testBit(am,i) :
+            for j in range(0,11):
+                if testBit(am,j) : allAlgoH[i][j]+=1 
+
+
 ntot=0
 nmu=0
+nmuhp=0
 n9=0
+n9all=0
+neall=0
 nhp=0
 n10=0
 ne=0
@@ -73,17 +105,70 @@ while True:
     except : break
     ntot+=1
     if float(values[mcfrac])<0.5 : continue
+#    if (not testAlgo(values,10)) : continue
+
+    fillAll(values)
+#    if earlyAlgo(values)==0 : print values
+    if (str2bool(values[hp])) :
+      algoH[int(values[algo])]+=1
+      oriAlgoH[int(values[oriAlgo])]+=1
+      earlyCountH[countEarly(values)]+=1
+
     nmu+=1
-    if (not testAlgo(values,9)) : continue
-    n9+=1
-    if (bool(values[hp])) : nhp+=1
-    if (testAlgo(values,10)) : n10+=1
+    if (not str2bool(values[hp])) : continue
+    nmuhp+=1
+    if (testAlgo(values,9)) : n9all+=1
+    if earlyAlgo(values) : neall+=1
+    if (not testAlgo(values,10)) : continue
+    n10+=1
+    if (str2bool(values[hp])) : nhp+=1
+    if (testAlgo(values,9)) : n9+=1
     if earlyAlgo(values) : ne+=1
     if promptAlgo(values) : npx+=1
     fill(npixH,values,npixels)
     fill(n3dH,values,n3d)
+    
 
-print ntot,nmu,n9,nhp,n10,ne,npx
+print ntot,nmu,nmuhp,n9all,neall,n10,nhp,n9,ne,npx
+
+allAlgoH /=(0.01*float(nmu))
+print oriAlgoH
+print allAlgoH
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.set_xlim(0,11)
+ax.set_ylim(0,11)
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+majorLocator   = MultipleLocator(1)
+ax.xaxis.set_major_locator(majorLocator)
+ax.yaxis.set_major_locator(majorLocator)
+for i in range(0,11) :
+    for j in range(i,11):
+        plt.annotate('%4.1f'%allAlgoH[i][j],xy=(i+0.25,j+0.25),
+                     # horizontalalignment='left', verticalalignment='bottom'
+                     )
+ax.set_title(sample,fontsize=20, fontweight='bold')
+plt.grid(True,which='both')
+#plt.legend(loc='upper right')
+plt.show()
+
+
+plt.step(algoX,algoH,where='post',label='final algo', linewidth=2)
+plt.step(algoX,oriAlgoH,where='post',label='original algo', linewidth=2)
+plt.grid(True)
+plt.title(sample,fontsize=20, fontweight='bold')
+plt.legend(loc='upper right')
+plt.show()
+
+plt.step(algoX,earlyCountH,where='post',label='count early', linewidth=2)
+print earlyCountH
+plt.grid(True)
+plt.title(sample,fontsize=20, fontweight='bold')
+plt.legend(loc='upper right')
+plt.show()
+
+
 
 y,x = np.histogram(npixH,np.linspace(0.,6.,7))
 y.resize(len(x))
--- trackHist.py
+++ trackHist.py
@@ -17,8 +17,8 @@ def find_lt(a, x):
     raise ValueError
 
 
-# cvsF = open('/Users/innocent/data/mva/ttbar.csv')
-cvsF = open('/Users/innocent/data/mva/ttbarNewMVA.csv')
+cvsF = open('/Users/innocent/data/mva/ttbar.csv')
+# cvsF = open('/Users/innocent/data/mva/ttbarNewMVA.csv')
 
 
 vtuple = csv.reader(cvsF)
@@ -30,8 +30,12 @@ dof = header.index('dof')
 mva = header.index('mva')
 mcfrac = header.index('mcfrac')
 algo = header.index('algo')
+oriAlgo = header.index('orialgo')
+eta = header.index('eta')
 pt = header.index('pt')
 n3d = header.index('3DLayers')
+nl = header.index('Layers')
+nm = header.index('missingLayers')
 hp = header.index('hp')
 
 
@@ -49,10 +53,21 @@ class fRate :
         _cAll,_bin_edges = np.histogram(self.all,bins)
         _cFake,_bin_edges = np.histogram(self.fake,bins)
         # _bin_centres = (_bin_edges[:-1] + _bin_edges[1:])/2.
-        _ratio = _cFake.astype(float,copy=False)/_cAll.astype(float,copy=False)
+        _ratio = _cFake.astype(float,copy=False)/np.fmax(_cAll.astype(float,copy=False),1.)
         _ratio.resize(len( _ratio)+1)
-        return (_ratio,_bin_edges)
+        _eff = _cAll.astype(float,copy=False)/float(len(self.all))
+        _eff.resize(len(_bin_edges))
+        return (_ratio,_bin_edges, _eff)
 
+    def cum(self,bins) :
+        _cAll,_bin_edges = np.histogram(self.all,bins)
+        _cFake,_bin_edges = np.histogram(self.fake,bins)
+        _sig = np.cumsum(_cAll, dtype=float)
+        _ratio = np.cumsum(_cFake, dtype=float)/_sig
+        _eff = _sig/float(len(self.all))
+        _ratio.resize(len(_bin_edges))
+        _eff.resize(len(_bin_edges))
+        return (_ratio,_bin_edges, _eff)
 
 class multiPlot :
     def __init__(self, zbins,zlab):
@@ -70,7 +85,7 @@ class multiPlot :
     def plot(self, bins) :
         k=0
         for h in self.hist:
-            _y,_x = h.ratio(bins=bins)
+            _y,_x,_z = h.ratio(bins=bins)
             try :
                 _label=self.zbins[k]
             except : _label='inf'
@@ -81,50 +96,142 @@ class multiPlot :
 
 ntot=0
 
+etaH=fRate()
 chi2H = fRate()
+chi2lH = fRate()
 n3dH = fRate()
+nlH = fRate()
+nmH = fRate()
 algoH = fRate()
+orialgoH = fRate()
 mvaHpt = multiPlot([0.25,.5,2.,4.],'pt')
 mvaH3d = multiPlot([1.1,2.1,3.1,4.1,6.1],'3d')
 mvaHal = multiPlot([0.1,1.1,2.1,3.1,4.1,5.1,6.1],'algo')
+etaHal = multiPlot([0.1,1.1,2.1,3.1,4.1,5.1,6.1],'algo')
 
 
+nhp=0
 while True:
     try :
         values = vtuple.next()
     except : break
     ntot+=1
-    if float(values[dof]) >0 :
-      chi2H.fill(float(values[chi2])/float(values[dof]),abs(float(values[mcfrac]))<0.5)
+    if  values[hp] != 'True' : continue
+    nhp+=1
+    if float(values[dof]) <0.5 : continue;
+    if float(values[nl])  <0.5 : continue;
+#    if float(values[pt]) <2 : continue
+#    if float(values[n3d])  <2.5 : continue;
+#    if float(values[chi2]) >0.4*float(values[dof]) : continue
+
+    etaH.fill(float(values[eta]),abs(float(values[mcfrac]))<0.5)
+    chi2H.fill(float(values[chi2])/float(values[dof]),abs(float(values[mcfrac]))<0.5)
+    chi2lH.fill(float(values[chi2])/(float(values[dof])*float(values[nl])),abs(float(values[mcfrac]))<0.5)
     # if float(values[pt]) >2 :
     n3dH.fill(float(values[n3d])+0.5, abs(float(values[mcfrac]))<0.5)
+    nlH.fill(float(values[nl])+0.5, abs(float(values[mcfrac]))<0.5)
+    nmH.fill(float(values[nm])+0.5, abs(float(values[mcfrac]))<0.5)
     algoH.fill(float(values[algo])+0.5, abs(float(values[mcfrac]))<0.5)
+    orialgoH.fill(float(values[oriAlgo])+0.5, abs(float(values[mcfrac]))<0.5)
     mvaHpt.fill(float(values[mva]),float(values[pt]),abs(float(values[mcfrac]))<0.5)
     mvaH3d.fill(float(values[mva]),float(values[n3d]),abs(float(values[mcfrac]))<0.5)
-    mvaHal.fill(float(values[mva]),float(values[algo]),abs(float(values[mcfrac]))<0.5)
+    mvaHal.fill(float(values[mva]),float(values[oriAlgo]),abs(float(values[mcfrac]))<0.5)
+    etaHal.fill(float(values[eta]),float(values[oriAlgo]),abs(float(values[mcfrac]))<0.5)
 
 
-print ntot
-y,x = chi2H.ratio(bins=np.linspace(0, 40, 81))
+print ntot,nhp
+
+y,x,z = etaH.ratio(bins=np.linspace(-2.5, 2.5, 51))
 print x
 print y
-plt.step(x,y,where='post')
+print z
+plt.step(x,y,where='post',label='eta fake')
+plt.step(x,z,where='post',label='eff')
+plt.grid(True)
+plt.legend(loc='upper right')
+#plt.yscale('log')
+plt.show()
+
+etaHal.plot(bins=np.linspace(-2.5, 2.5, 51))
+plt.grid(True)
+plt.legend(loc='upper right')
+plt.show()
+
+
+y,x,z = chi2H.cum(bins=np.linspace(0, 10, 81))
+print x
+print y
+print z
+plt.step(x,y,where='post',label='chi2 fake')
+plt.step(x,z,where='post',label='chi2 eff')
+plt.grid(True)
+plt.legend(loc='upper right')
+#plt.yscale('log')
+plt.show()
+
+
+
+
+y,x,z = chi2lH.cum(bins=np.linspace(0, 1, 81))
+print x
+print y
+plt.step(x,y,where='post',label='chi2/nlayer')
+plt.step(x,z,where='post',label='eff')
+plt.grid(True)
+plt.legend(loc='upper right')
+#plt.yscale('log')
+plt.show()
+
+
+
+y,x,z = n3dH.ratio(bins=np.linspace(0., 12., 13))
+print x
+print y
+plt.step(x,y,where='post',label='3dLayers')
+plt.step(x,z,where='post',label='eff')
+plt.legend(loc='upper right')
 plt.grid(True)
 plt.show()
 
-y,x = n3dH.ratio(bins=np.linspace(0., 12., 13))
+y,x,z = nlH.ratio(bins=np.linspace(0., 20., 21))
 print x
 print y
-plt.step(x,y,where='post')
+plt.step(x,y,where='post',label='Layers')
+plt.step(x,z,where='post',label='eff')
+plt.legend(loc='upper right')
 plt.grid(True)
 plt.show()
 
-y,x = algoH.ratio(bins=np.linspace(0., 12., 13))
+y,x,z = nmH.ratio(bins=np.linspace(0., 12., 13))
 print x
 print y
-plt.step(x,y,where='post')
+plt.step(x,y,where='post',label='missing Layers')
+plt.step(x,z,where='post',label='eff')
+plt.legend(loc='upper right')
 plt.grid(True)
 plt.show()
+
+
+y,x,z = algoH.ratio(bins=np.linspace(0., 12., 13))
+print x
+print y
+plt.step(x,y,where='post',label='algo')
+plt.step(x,z,where='post',label='eff')
+plt.legend(loc='upper right')
+plt.grid(True)
+plt.show()
+
+y,x,z = orialgoH.ratio(bins=np.linspace(0., 12., 13))
+print x
+print y
+plt.step(x,y,where='post',label='ori-algo')
+plt.step(x,z,where='post',label='eff')
+plt.legend(loc='upper right')
+plt.grid(True)
+plt.show()
+
+
+
 
 
 mvaHpt.plot(bins=np.linspace(-1, 1,26))
