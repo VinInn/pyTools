f4547ad4ba2db6e98a4ffa00f82e51c1390cd374
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2016-06-25 18:00:01 +0200

    a couple of simple table builders

---
+++ perfReport.py
@@ -0,0 +1,18 @@
+d = {}
+ori= 'perfStat_ori.txt'
+pgo = 'perfStat_pgo.txt'
+with open(ori) as f:
+    for line in f:
+       (val, key) = line.split()
+       if not key in d : d[key] = [0.,0.]
+       d[key][0] = float(val)
+
+with open(pgo) as f:
+    for line in f:
+       (val, key) = line.split()
+       if not key in d : d[key] = [0.,0.]
+       d[key][1] = float(val)
+
+# print d
+for k,c in d.iteritems() :
+  print '|',k,'| ',"{:15.0f}".format(c[0]),'| ',"{:15.0f}".format(c[1]),'| ', "{:6.2f}".format(100*(c[0]-c[1])/c[0]),'||'
--- simpleDumpTracks.py
+++ simpleDumpTracks.py
@@ -3,23 +3,29 @@ from ROOT import gROOT, gStyle, TCanvas, TF1, TFile, TTree, gRandom, TH1F, TH2F
 import os
 
 
-#ver ='81X'
+ver ='81X'
 #ver ='810'
-ver ='80X'
+#ver ='80X'
 ##ver ='807'
 #ver ='767'
-eventsRef = Events("reco"+ver+".root")
+#ver ="ncl"
+#ver ="HiM"
+eventsRef = Events("SingleMuon22events_reco_81X.root")
+#eventsRef = Events('SingleMuon22events_reco_newcal.root')
+#eventsRef = Events('SingleMuon22events_reco_HM.root')
 
 tracksRef = Handle("std::vector<reco::Track>")
-label = "ctfWithMaterialTracksP5"
+label = "generalTracks"
 #quality = "highPurity"
 #quality = "tight"
 #quality = "loose"
 
+def format(x) :
+  return '{:.2f}'.format(x)
 
 
-#for i in range(0, eventsRef.size()):
-for i in range(0, 200):
+for i in range(0, eventsRef.size()):
+#for i in range(0, 200):
   a= eventsRef.to(i)
   ev = eventsRef.eventAuxiliary().event()
 #  print "Event", i 
@@ -28,13 +34,13 @@ for i in range(0, 200):
   for track in tracksRef.product():
 #   if (track.phi()<0) : continue
 #   if (track.eta()<0) : continue
-#   if (track.pt()<5) : continue
+    if (track.pt()<50) : continue
 #   if (track.quality(track.qualityByName(quality))) :
 #    pattern = track.hitPattern()
 #    if (pattern.numberOfValidHits() != (pattern.numberOfValidPixelHits()+pattern.numberOfValidStripHits())) :
 #      print pattern.numberOfValidHits(),pattern.numberOfValidPixelHits(),pattern.numberOfValidStripHits(), pattern.numberOfValidPixelHits()+pattern.numberOfValidStripHits()
-    trVal.append([ev, ver, 10*int(100*track.eta())+track.phi(), track.eta(), track.phi(), track.pt(),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), 
-                  track.numberOfLostHits(), track.validFraction(), track.ndof(),track.chi2()])
+    trVal.append([i, ev, 10*int(100*track.eta())+track.phi(), ver, format(track.eta()), format(track.phi()), format(track.pt()),  track.numberOfValidHits() , track.hitPattern().numberOfValidPixelHits(), 
+                  track.numberOfLostHits(), format(track.validFraction()), track.ndof(),format(track.chi2()),track.algo()-4,track.originalAlgo()-4,track.quality(track.qualityByName("highPurity"))])
 
 #  print tracksRef.product().size(), len(trVal)
   for tr in trVal :
