dae06cf2a54807af4d2b7e603aebf865ac9b0ad4
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2017-11-12 16:13:25 +0100

    more utils

--- FWLiteUtils.py
+++ FWLiteUtils.py
@@ -1,5 +1,15 @@
 from DataFormats.FWLite import Handle, Events
 from bisect import bisect_left, bisect_right
+import inspect
+
+#file prefixes
+xrd = 'root://cms-xrd-global.cern.ch//'
+tier0 = 'root://cms-xrd-tzero.cern.ch//' # 'file:/eos/cms/tier0'
+tier2 = 'root://eoscms.cern.ch//' #'file:/eos/cms'
+myTracking = 'root://eoscms.cern.ch///store/group/phys_tracking/vincenzo/run2017/'
+
+def fullFileName(prefix,files) :
+    return map(lambda x : prefix+x,files)
 
 def runid(event,lumi) :
     id = event.object().id()
@@ -26,8 +36,22 @@ class Lumi:
     def __len__(self):
         return int(self.events.size())
 
-def eventInLumiRange(ev,l,h):
+def eventsInLumiRange(ev,l,h):
    ls = Lumi(ev)
    il = bisect_left(ls,l)
    return (il,bisect_right(ls,h,il))
+
+
+class Mini :
+  def __init__(self):
+    self.jets = (Handle('vector<pat::Jet>'),"slimmedJets")
+    self.lumi = (Handle("std::vector<LumiScalers>"),"scalersRawToDigi")
+    self.vertices = (Handle("vector<reco::Vertex>"),"offlineSlimmedPrimaryVertices") 
+    self.cand = (Handle("vector<pat::PackedCandidate>") ,"packedPFCandidates")
+    self.ltk =	(Handle("vector<pat::PackedCandidate>") ,"lostTracks" )
+
+  def set(self,ev) :
+    for m in inspect.getmembers(self, lambda x : not inspect.ismethod(x)) :
+       if (m[0][0]!='_') : a=ev.getByLabel(m[1][1],m[1][0])
+
 
